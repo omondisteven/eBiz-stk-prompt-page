d3c72a7977a81a190eff32de26eeb526607afd2b
omondisteven <omondisteven@gmail.com>
2025-04-21 22:18:12 +0300

    fixing send money and withdraw money transaction types

--- src/pages/api/stk_api/agent_stk_api.ts
+++ src/pages/api/stk_api/agent_stk_api.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         BusinessShortCode,
         Password,
         Timestamp,
-        TransactionType: 'CustomerBuyGoodsOnline',
+        TransactionType: 'CustomerWithdrawal',
         Amount: amount,
         PartyA: phone,
         PartyB: BusinessShortCode,
--- src/pages/api/stk_api/sendmoney_stk_api.ts
+++ src/pages/api/stk_api/sendmoney_stk_api.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         BusinessShortCode,
         Password,
         Timestamp,
-        TransactionType: 'CustomerPayBillOnline',
+        TransactionType: 'CustomerTransferSender',
         Amount: amount,
         PartyA: phone,
         PartyB: BusinessShortCode ,
