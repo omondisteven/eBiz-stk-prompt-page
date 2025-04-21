7a06ff78237ec20dcf22fe04ec58c8bad88c3ac1
omondisteven <omondisteven@gmail.com>
2025-04-22 01:58:12 +0300

    updated transaction text

--- src/pages/ThankYouPage.tsx
+++ src/pages/ThankYouPage.tsx
@@ -21,7 +21,7 @@ const ThankYouPage = () => {
           Thank you for using M-Poster payment Platform for M-Pesa
         </h1>
         <h4 className="text-2xl text-green-600 font-bold mb-6">
-          If you cancelled the &quot;Enter PIN&quot; prompt and you still wish to make corrections to your entries, you may go BACK to the payments page or EXIT and SCAN the Qr code again.
+          If you cancelled the &quot;Enter PIN&quot; prompt and you still wish to proceed with the transaction, you may go BACK to the payments page or EXIT and SCAN the Qr code again.
         </h4>
         <div className="flex justify-center gap-4">
           <Button 
