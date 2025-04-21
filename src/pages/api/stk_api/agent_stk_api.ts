9b82b6dcfe2ee2675cc8194da6e85655ef6d0aa4
omondisteven <omondisteven@gmail.com>
2025-04-21 20:49:25 +0300

    fixing withdraw money

--- src/pages/api/stk_api/agent_stk_api.ts
+++ src/pages/api/stk_api/agent_stk_api.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         BusinessShortCode,
         Password,
         Timestamp,
-        TransactionType: 'CustomerToAgentTransfer',
+        TransactionType: 'CustomerBuyGoodsOnline',
         Amount: amount,
         PartyA: phone,
         PartyB: BusinessShortCode,
