8979466990fe90471ca916a3c67f9e7d3d2d0e4d
omondisteven <omondisteven@gmail.com>
2025-05-22 14:00:39 +0300

    adding close button to receipt

--- src/pages/ThankYouPage.tsx
+++ src/pages/ThankYouPage.tsx
@@ -236,7 +236,7 @@ const ThankYouPage = () => {
               className="absolute top-2 right-2 p-1 text-gray-500 hover:text-gray-700 rounded-full hover:bg-gray-100"
               aria-label="Close"
             >
-              <p>close receipt</p>
+              {/* <p>close receipt</p> */}
               <X className="w-5 h-5" />
             </button>
           <div className="flex justify-center mb-4">
