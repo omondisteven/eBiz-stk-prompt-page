92c31c87dd531f3122fef7d91226307ecd252b99
omondisteven <omondisteven@gmail.com>
2025-05-30 14:17:34 +0300

    Fixing history page

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -1,5 +1,5 @@
 // src/components/TransactionTable.tsx
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Badge } from '@/components/ui/Badge';
 import { format } from 'date-fns';
 import { useAppContext } from '@/context/AppContext';
